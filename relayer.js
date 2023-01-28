57e3544b00e5465db7b3079d86809ab63f6c22a3
Developer Uche <nweleuchenna2004@gmail.com>
2023-01-28 22:57:34 +0100

    developeruche: started working on the bridge relayer

---
+++ relayer.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+// =======================================
+// CONSTANTS and VARIABLES
+// =======================================
+let FVM = {
+    last_block_timestamp: 0,
+    provider: "https://api.hyperspace.node.glif.io/rpc/v1",
+    chain_id: "3141",
+    symbol: "tFil",
+    explorer: "https://hyperspace.filfox.info/"
+}
+
+let BSC = {
+    last_block_timestamp: 0,
+    provider: "https://endpoints.omniatech.io/v1/bsc/testnet/public",
+    chain_id: "97",
+    symbol: "tBNB",
+    explorer: "https://testnet.bscscan.com/"
+}
+
+
